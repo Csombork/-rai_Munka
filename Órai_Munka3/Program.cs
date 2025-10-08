a1051842caad704ad35f178630391afbd821cd90
Csombork <levente.csombor.szlany@umszki.hu>
2025-10-08 12:10:33 +0200

    Fullos órai munka

--- Órai_Munka3/Program.cs
+++ Órai_Munka3/Program.cs
@@ -3,17 +3,22 @@ int szam = int.Parse(Console.ReadLine());
 Console.WriteLine("\nA szám osztói:");
 
 bool prím = true;
+string osztói = "";
 
 for (int i = 2; i < szam; i++)
 {
     if (szam % i == 0)
     {
-        Console.WriteLine(i);
         prím = false;
+        osztói += i + " ";
     }
 }
 
 if (prím)
 {
     Console.WriteLine("Ez egy prím szám!");
+}
+else
+{
+    Console.WriteLine(osztói);
 }
---
+++ Órai_Munka4/Program.cs
@@ -0,0 +1,15 @@
+﻿for (int i = 1; i <= 9; i++)
+{
+    Console.WriteLine();
+    for (int j = 1;j <= 9;j++)
+    {
+        if ((i * j) / 10 == 0)
+        {
+            Console.Write(i * j + "  ");
+        }
+        else
+        {
+            Console.Write(i * j + " ");
+        }
+    }
+}
