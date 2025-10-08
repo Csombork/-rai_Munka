d0c681a85164843afaa3b54af4354901f60dd396
Csombork <levente.csombor.szlany@umszki.hu>
2025-10-08 10:24:50 +0200

    órai meló

--- Órai_Munka/Program.cs
+++ Órai_Munka/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Írj be egy számot:");
+int szam = int.Parse(Console.ReadLine());
+
+int hossz = 0;
+
+while (szam >= 1)
+{
+    szam /= 10;
+    hossz++;
+}
+
+Console.WriteLine($"Ennyi karakterből áll a számod: {hossz}");
---
+++ Órai_Munka2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Írj be egy egész számot, ami 8 jegyű lesz és egy TAJ számot jelöl:");
+int TAJ = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int utsószámjegy = TAJ % 10;
+TAJ = TAJ / 10;
+int összeg = 0;
+
+for (int i = 1; i < 9; i++)
+{
+    int számjegy = TAJ % 10;
+    
+    if (i % 2 == 0)
+    {
+        számjegy *= 3;
+    }
+    else
+    {
+        számjegy *= 7;
+    }
+    összeg += számjegy;
+    TAJ = TAJ / 10;
+}
+
+if (összeg % 10 == TAJ)
+{
+    Console.WriteLine("Ez egy valid TAJ szám!");
+}
+else
+{
+    Console.WriteLine("Érvénytelen TAJ szám!");
+}
+
+Console.WriteLine(összeg);
+Console.WriteLine(TAJ);
