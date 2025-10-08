34e7d6433aae82b38874b32ad1573ca7866ea173
Csombork <levente.csombor.szlany@umszki.hu>
2025-10-08 11:45:07 +0200

    Sanyiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

---
+++ Órai_Munka3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Adj meg egy számot: ");
+int szam = int.Parse(Console.ReadLine());
+Console.WriteLine("\nA szám osztói:");
+
+bool prím = true;
+
+for (int i = 2; i < szam; i++)
+{
+    if (szam % i == 0)
+    {
+        Console.WriteLine(i);
+        prím = false;
+    }
+}
+
+if (prím)
+{
+    Console.WriteLine("Ez egy prím szám!");
+}
