afb845710560904697ec420852169ff536179477
Csombork <levente.csombor.szlany@umszki.hu>
2025-10-14 10:34:52 +0200

    mászkál az X

---
+++ Game/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+int hight = 8;
+int widht = 8;
+int jh = 0;
+int jw = 0;
+
+Console.CursorVisible = false;
+
+while (true)
+{
+    var beolvasott = Console.ReadKey();
+    Console.Clear();
+
+    if (beolvasott.Key == ConsoleKey.S && jh < Console.WindowHeight - 1)
+    {
+        jh++;
+    }
+    if (beolvasott.Key == ConsoleKey.W && jh > 0)
+    {
+        jh--;
+    }
+    if (beolvasott.Key == ConsoleKey.A && jw > 0)
+    {
+        jw--;
+    }
+    if (beolvasott.Key == ConsoleKey.D && jw < Console.WindowWidth - 1)
+    {
+        jw++;
+    }
+    Console.CursorLeft = jw;
+    Console.CursorTop = jh;
+    Console.WriteLine("X");
+}
---
+++ Órai_Munka5/Program.cs
@@ -0,0 +1,29 @@
+﻿int width = 8; // A pálya adatai
+int height = 8;
+
+int row = 0; // A játékos adatai
+int col = 0;
+
+Console.CursorVisible = false;
+
+Console.WriteLine("X");
+
+Console.CursorLeft = 5;
+Console.CursorTop = 5;
+
+Console.Write("X");
+
+var mitNyomottMeg = Console.ReadKey();
+
+Console.WriteLine("\nEzt nyomtad meg:");
+Console.WriteLine(mitNyomottMeg.Key);
+
+if (mitNyomottMeg.Key == ConsoleKey.W)
+{
+    Console.WriteLine("WWWWW");
+}
+
+if (mitNyomottMeg.Key == ConsoleKey.L)
+{
+    Console.WriteLine("LLLLLLLL");
+}
