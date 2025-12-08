cd4328e600228c26e11b1468715d7b043dd6eb91
Csombor Levente Szlány <levente.csombor.szlany@umszki.hu>
2025-12-08 11:16:29 +0100

    asdfsd

---
+++ Óraimunka6/Program.cs
@@ -0,0 +1,33 @@
+﻿Random veletlen = new Random();
+
+int RandomSzam(int minimum = 0, int maximum = int.MaxValue)
+{
+    int szam = veletlen.Next(minimum, maximum + 1);
+    return szam;
+}
+
+
+int Számjegy()
+{
+    int szam = veletlen.Next(1, 10);
+    return szam;
+}
+
+int Százalék()
+{
+    int szam = veletlen.Next(0,101);
+    return szam;
+}
+
+bool Pénzérme()
+{
+    int szam = veletlen.Next(0,2);
+    if (szam == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+Console.WriteLine(Pénzérme());
