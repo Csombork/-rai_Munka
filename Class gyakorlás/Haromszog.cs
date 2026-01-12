c8fb03fbda7a4fe7042f1e1750f1a4c6fbe29aac
Csombork <levente.csombor.szlany@umszki.hu>
2026-01-12 11:24:09 +0100

    frfsd

---
+++ Class gyakorlás/Haromszog.cs
@@ -0,0 +1,68 @@
+﻿internal class Haromszog
+{
+    private int a;
+    private int b;
+    private int c;
+
+    public Haromszog(int a, int b, int c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public bool Szerkesztheto()
+    {
+        if ((a + b > c) && (a + c > b) && (b + c > a))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public int Kerulet()
+    {
+        int k = a + b + c;
+        return k;
+    }
+
+    public int Terulet()
+    {
+        int s = (a + b + c) / 2;
+        int t = (int)Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        return t;
+    }
+
+    public bool EgyenloSzaru()
+    {
+        if (b == c || a == c || b == c)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool Szabalyos()
+    {
+        if (a != b || a != c)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool Derekszogu()
+    {
+        if (a * a + b * b == c * c)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public string Szovegesen()
+    {
+        string válasz = $"{a}, {b}, {c}";
+        return válasz;
+    }
+}
---
+++ Class gyakorlás/Program.cs
@@ -0,0 +1,8 @@
+﻿Random r = new Random();
+
+for (int i = 0; i < 100; i++) // 100 hármszög generálása
+{
+    int a = r.Next(1, 101);
+    int b = r.Next(1, 101);
+    int c = r.Next(1, 101);
+}
