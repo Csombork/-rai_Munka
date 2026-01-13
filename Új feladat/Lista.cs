627731c7aa0bdb45d0330ae16fc1ce2e047510c0
Csombork <levente.csombor.szlany@umszki.hu>
2026-01-13 12:10:41 +0100

    Listázunk!!!

--- Új feladat/Lista.cs
+++ Új feladat/Lista.cs
@@ -23,25 +23,98 @@ namespace Új_feladat
             this.tomb = new int[kapacitás];
         }
 
-        public void Hozzáadás(int adat)
+        public void Hozzáadás(int adat) // Hozzá adsz egy számot a tömbhöz
         {
             if (táelmszám + 1 > kapacitás)
             {
-                kapacitás = kapacitás * 2;
-                int[] mentés = new int[kapacitás];
-                for (int i = 0; i < táelmszám; i++)
-                {
-                    mentés[i] = tomb[i]; 
-                }
-                tomb = mentés;
+                Bővítés();
             }
             tomb[táelmszám] = adat;
             táelmszám++;
         }
 
+        
+        private void Bővítés() // Rejtett segéd fügvény
+        {
+            kapacitás = kapacitás * 2;
+            int[] mentés = new int[kapacitás];
+            for (int i = 0; i < táelmszám; i++)
+            {
+                mentés[i] = tomb[i];
+            }
+            tomb = mentés;
+        }
+
         public int Kiír(int hanyelem)
         {
             return tomb[hanyelem];
+        }
+
+        public void TötölAdottÉrtéket()
+        {
+
+        }
+
+        public void TötölAdottIndexet()
+        {
+
+        }
+
+        public void Rendez(int valami)
+        {
+
+        }
+
+        public void Hozzáadás(int[] uj)
+        {
+            foreach (var item in uj)
+            {
+                Hozzáadás(item);
+            }
+        }
+
+        public void Kiürít()
+        {
+            tomb = new int[kapacitás];
+            táelmszám = 0;
+        }
+
+        public int Osszeg()
+        {
+            int összeg = 0;
+            for (int i = 0; i < táelmszám; i++)
+            {
+                összeg += tomb[i];
+            }
+            return összeg;
+        }
+
+        public bool Tartalmaz(int keresett)
+        {
+            for (int i = 0; i < táelmszám; i++)
+            {
+                if (tomb[i] == keresett)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int Index(int keresett)
+        {
+            /// <summary>
+            /// Megkeresi az indexét a megadott
+            /// számnak
+            /// </summary>
+            for (int i = 0; i < táelmszám; i++)
+            {
+                if (tomb[i] == keresett)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
--- Új feladat/Program.cs
+++ Új feladat/Program.cs
@@ -1,11 +1,34 @@
 ﻿using Új_feladat;
 
 Lista lista = new Lista();
+Lista szöveg = new Lista();
+List<int> list = [];
+
+list.Add(58);
 
 
+int[] tomb = [1, 2, 3, 4];
+
+lista.Hozzáadás(tomb);
 
 for (int i = 0; i < 100; i++)
 {
     lista.Hozzáadás(i);
     Console.WriteLine(lista.Kiír(i));
 }
+
+Console.WriteLine("Összeg");
+Console.WriteLine(lista.Osszeg());
+Console.WriteLine();
+
+Console.WriteLine("Hibás");
+Console.WriteLine(lista.Tartalmaz(123456));
+Console.WriteLine(lista.Index(123456));
+
+Console.WriteLine("\nHelyes");
+Console.WriteLine(lista.Tartalmaz(10));
+Console.WriteLine(lista.Index(10));
+
+
+
+
