8ea78df5931b670096d9b6bb816d1f5ee51726f1
Csombork <levente.csombor.szlany@umszki.hu>
2026-01-13 10:51:31 +0100

    lista

---
+++ Új feladat/Lista.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Új_feladat
+{
+    internal class Lista
+    {
+        // privát mezők
+        private int[] tomb;
+
+        private int kapacitás;
+
+        private int táelmszám;
+
+        // Tömb létrehozása és méretének megadása
+        public Lista(int kapacitás = 2)
+        {
+            this.kapacitás = kapacitás;
+            this.táelmszám = 0;
+            this.tomb = new int[kapacitás];
+        }
+    }
+}
